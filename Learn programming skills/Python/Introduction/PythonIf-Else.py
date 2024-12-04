0a8ab2a37cd4e950d4961acb7d2dc3fb4d271300
jabez-abija2399 <jabeznew2399@gmail.com>
2024-12-04 12:28:38 +0300

    Refactor PythonIf-Else script to improve input handling and conditional logic

--- Learn programming skills/Python/Introduction/PythonIf-Else.py
+++ Learn programming skills/Python/Introduction/PythonIf-Else.py
@@ -1,4 +1,4 @@
-
+#!/bin/python3
 
 import math
 import os
@@ -6,24 +6,16 @@ import random
 import re
 import sys
 
-n = random.randint(1, 100)  # Random integer between 1 and 100
-n=4
-print(n)
-odd = n % 2 != 0
-print(odd)
-even = n % 2 == 0
-print("even",even)
-
-if odd:
-    print("Weird")
-elif even >= 2 and even <= 5:
-    print("Not Weird")
-
-
-# Given an integer, n, perform the following conditional actions:
-
-# If n is odd, print Weird
-# If n is even and in the inclusive range of  to , print Not Weird
-# If n is even and in the inclusive range of  to , print Weird
-# If n is even and greater than , print Not Weird
-# Input Format
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if n % 2 != 0:
+        print("Weird")
+    elif 2 <=  n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+        
+    # print("Weird" if n % 2 != 0 or (6 <= n <= 20) else "Not Weird")
